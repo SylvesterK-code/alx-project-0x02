4ccd4d99d3c9d4f8a3cdf02f49f9656bfce6506e
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-07-26 23:46:45 +0000

    add Implement Navigation and Layout

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -24,22 +24,27 @@
 
 // pages/about.tsx
 
-import Layout from "@/components/layout/Layout";
+//import Layout from "@/components/layout/Layout";
 import React from 'react';
 import Button from '@/components/common/Button';
+import Header from "@/components/layout/Header";
 
 const AboutPage = () => {
   return (
-    <Layout>
+    <>
     <div className="p-8 space-y-4">
+      <Header /> 
+      <main className="p-6">
+
       <h1 className="text-3xl font-bold mb-4">About Page</h1>
       <div className="flex gap-4 flex-wrap">
         <Button title="Small Button" size="small" shape="rounded-sm" />
         <Button title="Medium Button" size="medium" shape="rounded-md" />
         <Button title="Large Button" size="large" shape="rounded-full" />
       </div>
+      </main>
     </div>
-    </Layout>
+    </>
   );
 };
 
