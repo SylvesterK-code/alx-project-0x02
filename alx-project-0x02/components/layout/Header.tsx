e6751e7650c689bebdf8c7234c2d045f656a654d
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-07-26 19:56:45 +0000

    add setup and Basic Routing in Next.js

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md p-4">
+      <h2 className="text-xl font-semibold text-gray-800">SoloForge Navigation</h2>
+    </header>
+  );
+};
+
+export default Header;
+
