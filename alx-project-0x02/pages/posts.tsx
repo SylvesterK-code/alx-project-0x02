eab7bc554b13d4de08f747518aad1a34c08c0666
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-07-27 01:16:31 +0000

    add Implement Navigation and Layout

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,14 @@
+// pages/posts.tsx
+
+import Layout from '@/components/layout/Layout';
+
+const PostsPage = () => {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold">Posts Page</h1>
+      <p className="mt-2">Here you will see all the posts in future updates.</p>
+    </Layout>
+  );
+};
+
+export default PostsPage;
